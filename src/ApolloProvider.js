93109ea10cb0526b43fcddca237d01d9f052e057
serafima <svp1982@mail.ru>
2021-03-30 16:52:42 +0200

    preperation commit

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -10,7 +10,7 @@ import {
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'https://mysterious-beyond-96868.herokuapp.com/',
 })
 
 const authLink = setContext(() => {
